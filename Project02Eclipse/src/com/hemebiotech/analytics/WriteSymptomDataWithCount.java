4d4f1c40bda95e4fbdeb4901cb021bf01e7d9bce
naziha.chebil@lifen.fr <72403363+nazihachebil@users.noreply.github.com>
2021-02-10 14:07:33 +0100

    Generate Results.out

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataWithCount.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataWithCount.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException; 
 import java.util.TreeMap; 
 import java.util.Set; 
-import java.util.Iterator; 
+import java.util.Iterator;
 import java.util.Map; 
 
 
